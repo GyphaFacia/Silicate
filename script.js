1507c4d2a2f43e272919254dcb265bef8e9bd665
Blank <holoparrentattachment@gmail.com>
2021-07-05 04:36:21 +0300

    Engine init

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
-var cnv, eng, ctx
-
 function getRes(){return vec(cnv.width, cnv.height)}
 function getCenter(){return getRes().div(2)}
 
 function canvasInit(){
-    cnv = document.body.addElement('game_canvas', 'canvas')
+    window.cnv = document.body.addElement('game_canvas', 'canvas')
     cnv.width = window.innerWidth
     cnv.height = window.innerHeight
-    ctx = cnv.getContext('2d')
+    window.ctx = cnv.getContext('2d')
     window.requestAnimationFrame(gameTick)
 }
 
+function engineInit(){
+    window.Engine = Matter.Engine
+    window.Render = Matter.Render
+    window.Runner = Matter.Runner
+    window.Bodies = Matter.Bodies
+    window.Composite = Matter.Composite
+    
+    window.engine = Engine.create()
+    
+    window.runner = Runner.create()
+    Runner.run(runner, engine)
+}
+
+function clearCanvas(){
+    ctx.clearRect(0, 0, ...getRes().$)
+}
+
 function gameTick(){
-    ctx.clearRect(0, 0, ...getRes().$);
+    clearCanvas()
+    
+    
+    
     window.requestAnimationFrame(gameTick)
 }
 
 function gameStart(){
     console.clear()
     canvasInit()
-    console.log(getRes().str);
-    console.log(getCenter().str);
+    engineInit()
 }
 setTimeout(()=>{gameStart()}, 100)
 
