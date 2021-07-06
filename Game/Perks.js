ab00daf7d8f18f802d9398d38a335766e0e65609
Blank <holoparrentattachment@gmail.com>
2021-07-06 09:29:04 +0300

    Perk icons reworked

--- Game/Perks.js
+++ Game/Perks.js
@@ -2,4 +2,58 @@ class Perk {
     constructor() {
         
     }
+    
+    get name(){return this.constructor.name}
+    
+    draw(){
+        // document.querySelector('.perks').addHtml(`
+        //     <div class="perk">
+    	// 		<img src="./src/Perks/${this.name}.svg" alt="" class="perk__img">
+    	// 		<div class="perk__cooldown"></div>
+    	// 	</div>
+        // `)
+        
+        document.querySelector('.perks').addHtml(`
+            <img src="./src/Perks/${this.name}.svg" alt="" class="perk">
+        `)
+    }
 }
+
+class SpawnSome extends Perk {
+    
+}
+
+let p = new SpawnSome()
+p.draw()
+p.draw()
+p.draw()
+p.draw()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
