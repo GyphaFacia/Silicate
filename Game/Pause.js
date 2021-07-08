3569512cb13d59ed8452936466d3b64cdcf79a4c
Blank <holoparrentattachment@gmail.com>
2021-07-08 13:10:55 +0300

    Pause module added

---
+++ Game/Pause.js
@@ -0,0 +1,52 @@
+function handlePause(){
+    PAUSED = !PAUSED
+    
+    if(PAUSED){
+        document.querySelector('.pause-menu').classList.remove('hide')
+        document.querySelector('.pause-button').classList.add('pause-button--paused')
+    }
+    else{
+        document.querySelector('.pause-menu').classList.add('hide')
+        document.querySelector('.pause-button').classList.remove('pause-button--paused')
+    }
+    
+    for(let perk of PERKS){
+        perk.cancel()
+    }
+    
+    if(ENTITIES.length < 100){
+        for(let host of [ENTITIES, CRISPS]){
+            for(let ent of host){
+                let static = PAUSED ? true : ent.stat
+                ent.stat = ent.isStatic()
+                Matter.Body.setStatic(ent.body, static)
+            }
+        }
+    }
+    
+    for(let perk of document.querySelectorAll('.perk')){
+        if(PAUSED){
+            perk.classList.add('hide')
+        }
+        else{
+            perk.classList.remove('hide')
+        }
+    }
+}
+
+function initPause(){
+    let pb = document.body.addElement('pause-button', 'Button')
+    let ico = pb.addElement('pause-button__ico')
+    ico.classList.add('far')
+    ico.classList.add('fa-pause-circle')
+    
+    document.querySelector('.pause-button').onclick = ()=>{
+        handlePause()
+    }
+    
+    document.onkeyup = (e)=>{
+        if(e.key === "Escape") {
+            handlePause()
+        }
+    }
+}
--- script.js
+++ script.js
@@ -13,52 +13,6 @@ function gameTick(){
     }
 }
 
-function handlePause(){
-    PAUSED = !PAUSED
-    
-    if(PAUSED){
-        document.querySelector('.pause-menu').classList.remove('hide')
-        document.querySelector('.pause-button').classList.add('pause-button--paused')
-    }
-    else{
-        document.querySelector('.pause-menu').classList.add('hide')
-        document.querySelector('.pause-button').classList.remove('pause-button--paused')
-    }
-    
-    for(let perk of PERKS){
-        perk.cancel()
-    }
-    
-    // for(let host of [ENTITIES, CRISPS]){
-    //     for(let ent of host){
-    //         let static = PAUSED ? true : ent.stat
-    //         ent.stat = ent.isStatic()
-    //         Matter.Body.setStatic(ent.body, static)
-    //     }
-    // }
-    
-    for(let perk of document.querySelectorAll('.perk')){
-        if(PAUSED){
-            perk.classList.add('hide')
-        }
-        else{
-            perk.classList.remove('hide')
-        }
-    }
-}
-
-function initPause(){
-    document.querySelector('.pause-button').onclick = ()=>{
-        handlePause()
-    }
-    
-    document.onkeyup = (e)=>{
-        if(e.key === "Escape") {
-            handlePause()
-        }
-    }
-}
-
 
 function gameStart(){
     canvasInit()
@@ -90,8 +44,6 @@ ply.spawnSilicate(getRes().mul(0.15, 0.5))
 
 // let bot = new Bot(2)
 // bot.spawnSilicate(getRes().mul(0.85, 0.5))
-
-let m = new Mush()
 
 // handlePause()
 
