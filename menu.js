469446db0f5d3a62613aa6d60a8a0f2b7258c2fe
Blank <holoparrentattachment@gmail.com>
2021-07-08 19:30:58 +0300

    Menu sil added

---
+++ menu.js
@@ -0,0 +1,100 @@
+let cnv = document.querySelector('canvas')
+ctx = cnv.getContext('2d')
+
+let scl = cnv.parentNode.getBoundingClientRect()
+
+cnv.width = scl.width
+cnv.height = scl.width
+
+var SIDES = 6
+var EYECLR = Clr(255, 200, 0)
+var COLOR = Clr(25)
+var RAD = cnv.width/2.5
+
+document.onmousemove = (e)=>{window.mousePos = vec(e.clientX, e.clientY)}
+function cursor(){return window.mousePos}
+window.mousePos = vec()
+
+setInterval(()=>{
+    ctx.beginPath()
+    ctx.clearRect(0, 0, cnv.width, cnv.height)
+    
+    ctx.fillStyle = COLOR.hex
+    ctx.filter = `drop-shadow(0 0 1px #fff) drop-shadow(0 0 25px #000)`
+    
+    ctx.save()
+    ctx.translate(cnv.width/2, cnv.height/2)
+        
+    let ang = time(5)
+    ctx.moveTo(...vec(sin(ang), cos(ang)).mul(RAD).$)
+    for(let i = 1; i < SIDES; i++){
+        let ang = 360/SIDES*i + time(5)
+        ctx.lineTo(...vec(sin(ang), cos(ang)).mul(RAD).$)
+    }
+    
+    ctx.closePath()
+    ctx.fill()
+    ctx.restore()
+    
+    ctx.filter = 'none'
+    let pos = vec(cnv.width/2)
+    let scl = vec(RAD)
+    
+    let eyeclr = EYECLR
+    
+    ctx.beginPath()
+    ctx.fillStyle = Hsl(15, 100, 95).hex
+    ctx.arc(...pos.$, scl.x/1.75, 0, 2*pi())
+    ctx.fill()
+    
+    pos = pos.add(cursor().sub(pos).ort.mul(scl.x/15))
+    
+    ctx.beginPath()
+    let eyeclr2 = Hsl(eyeclr.h, eyeclr.s, eyeclr.l-15)
+    ctx.fillStyle = eyeclr2.hex
+    ctx.arc(...pos.$, scl.x/2.25, 0, 2*pi())
+    ctx.fill()
+    
+    ctx.beginPath()
+    ctx.fillStyle = eyeclr.hex
+    ctx.arc(...pos.$, scl.x/2.5, 0, 2*pi())
+    ctx.fill()
+    
+    pos = pos.add(cursor().sub(pos).ort.mul(scl.x/20))
+    
+    ctx.beginPath()
+    ctx.fillStyle = '#000'
+    ctx.arc(...pos.$, scl.x/3, 0, 2*pi())
+    ctx.fill()
+    
+    ctx.beginPath()
+    ctx.fillStyle = '#fffe'
+    ctx.arc(...pos.sub(scl.x*0.1).$, scl.x/7, 0, 2*pi())
+    ctx.fill()
+}, 16)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
